63974e223edf012c3b939787fd639efd2ee4460b
opsb <oliver@opsb.co.uk>
2014-07-02 15:24:15 -0500

    Only run compile once

--- test/compile_test.sh
+++ test/compile_test.sh
@@ -1,30 +1,34 @@
 source "$BUILDPACK_TEST_RUNNER_HOME/test/support/test-utils"
 
-testCopiesContentsOfApiToBuildDir() 
+shouldCopyApiToBuild()
 {
-	compile $BUILD_DIR
 	assertCaptured "-----> Copying api to build"
 	assertTrue "Should have copied public folder to build dir" '[ -d "$BUILD_DIR/public" ]'
 	assertTrue "Should have copied Gemfile to build dir" '[ -f "$BUILD_DIR/Gemfile" ]'
 }
 
-testInstallsNode()
+shouldInstallNode()
 {
-	compile $BUILD_DIR
 	assertCaptured "-----> Resolved node version: 0.10.29"
 	assertTrue "Should have installed node" '[ -d "$BUILD_DIR/node-v0.10.29-linux-x64" ]'
 }
 
-testInstallNodeModules()
+shouldInstallNodeModules()
 {
-	compile $BUILD_DIR
 	assertTrue "Should have added node modules to ember app" '[ -d "$BUILD_DIR/app/node_modules" ]'
 }
 
-testInstallsBowerModules()
+shouldInstallBowerModules()
 {
-	compile $BUILD_DIR
 	assertTrue "Should have installed bower modules to ember app" '[ -d "$BUILD_DIR/app/vendor" ]'
 }
 
+testCompile() 
+{
+	compile $BUILD_DIR
 
+	shouldInstallNode
+	shouldInstallNodeModules
+	shouldInstallBowerModules	
+	shouldCopyApiToBuild	
+}
