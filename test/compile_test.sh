e31c3087156c31dc921c714d0f6e9c9e838ed5ef
opsb <oliver@opsb.co.uk>
2014-07-02 13:11:14 -0500

    Test node version is resolved correctly

--- test/compile_test.sh
+++ test/compile_test.sh
@@ -1,9 +1,16 @@
 source "$BUILDPACK_TEST_RUNNER_HOME/test/support/test-utils"
 
-testCopiesContentsOfApiToBuildDir() {
+testCopiesContentsOfApiToBuildDir() 
+{
 	compile $BUILD_DIR
 	assertCaptured "-----> Copying api to build"
 	assertTrue '[ -d "$BUILD_DIR/public" ]'
 	assertTrue '[ -f "$BUILD_DIR/Gemfile" ]'
+}
+
+testInstallsNode()
+{
+	compile $BUILD_DIR
+	assertCaptured "-----> Installing node 0.10.29"
 }
 
