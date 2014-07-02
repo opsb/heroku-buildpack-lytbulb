cb5c8e0b87f414e9924b41e6ee1ead882629ba13
opsb <oliver@opsb.co.uk>
2014-07-02 12:54:01 -0500

    Use status from common.sh

--- test/compile_test.sh
+++ test/compile_test.sh
@@ -2,7 +2,7 @@ source "$BUILDPACK_TEST_RUNNER_HOME/test/support/test-utils"
 
 testCopiesContentsOfApiToBuildDir() {
 	compile $BUILD_DIR
-	assertCaptured "  >> Copying api to build"
+	assertCaptured "-----> Copying api to build"
 	assertTrue '[ -d "$BUILD_DIR/public" ]'
 	assertTrue '[ -f "$BUILD_DIR/Gemfile" ]'
 }
